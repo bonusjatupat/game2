5412f0ad1e1ddbc49fe6fa9560f6477dd190cf2a
bonusjatupat <jatupat.bonus@mail.kmutt.ac.th>
2017-04-03 23:02:13 +0700

    fixed ground

--- Shark-eiei/GroundSet.java
+++ Shark-eiei/GroundSet.java
@@ -1,6 +1,8 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class GroundSet extends Ground
 {
+    public void act(){
+    }
     public GroundSet(){
         GreenfootImage groundSet = new GreenfootImage("groundSet.png");
         setImage(groundSet); // set image to this class
--- Shark-eiei/TreeSet.java
+++ Shark-eiei/TreeSet.java
@@ -1,6 +1,9 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class TreeSet extends Trees
 {
+    public void act(){
+    
+    }
     public TreeSet(){ // default constructor
         GreenfootImage treeSet = new GreenfootImage("treeSet.png"); // create variable to store image
         setImage(treeSet); // set it
